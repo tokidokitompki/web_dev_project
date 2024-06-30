1d0ead268bf0100e01baf11dd01d0747a1581d6c
tokidokitompki <ttompki5@students.kennesaw.edu>
2024-06-29 23:34:18 -0400

    changes

---
+++ public/javascript/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var path = require('path'); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public'); // send HTML file on GET request
+});
+
+app.post('/submit-form', (req, res) => {
+    const name = req.body.name; // access form data
+    // Add validation logic here
+    res.send(`Form Submitted!`);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
