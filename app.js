4d99b91bbc2b2c5ca923728085066852948175ec
tokidokitompki <ttompki5@students.kennesaw.edu>
2024-06-30 13:54:38 -0400

    update

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/contactform.html');
  
 });
 
 app.post('/submit-form', (req, res) => {
-    const username = req.body.username;
-    res.send(`Username is $ttompki5`);
+    const data = req.body;
+
+    console.log("Your Name: ", data.name);
+    console.log("Your Email: ", data.email);
+    console.log("Your Phone Number: ", data.phone);
+    console.log("Your Message: ", data.message)
+    res.send();
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${post}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
 
